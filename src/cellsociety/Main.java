01a436039acc9afca8ee5b95cdb877c9d8c5c558
Kyra Chan <ktc22@duke.edu>
2020-01-27 21:04:12 -0500

    Added visualization methods and main class

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,13 +1,29 @@
 package cellsociety;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+
 /**
- * Feel free to completely change this code or delete it entirely. 
+ * Feel free to completely change this code or delete it entirely.
  */
-public class Main {
+public class Main extends Application {
     /**
      * Start of the program.
      */
+    public static final String TITLE = "Cell Society";
+
+    // Initialize what will be displayed and how it will be updated.
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setResizable(false);
+        primaryStage.setTitle(TITLE);
+        Scene animationScene = Visualization.buildAnimationScene(primaryStage);
+
+    }
+
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        launch(args);
     }
 }
--- src/cellsociety/Visualization.java
+++ src/cellsociety/Visualization.java
@@ -1,4 +1,64 @@
 package cellsociety;
 
+import javafx.animation.Timeline;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
 public class Visualization {
+    public static final int SCREEN_WIDTH = 1200;
+    public static final int SCREEN_HEIGHT = 800;
+    public static final Paint SCREEN_BACKGROUND = Color.web("1f2e50");
+
+    private Scene splashScene;
+    private Timeline timeline;
+
+    public Scene buildAnimationScene(Stage primaryStage) {
+        ToolBar toolBar = buildToolBar(primaryStage, timeline);
+        HBox root = new HBox();
+        root.getChildren().addAll(toolBar);
+        return new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
+    }
+
+    private ToolBar buildToolBar(Stage primaryStage, Timeline timeline) {
+        ToolBar toolBar = new ToolBar();
+        toolBar.setOrientation(Orientation.VERTICAL);
+        Button buttonHome = createButton("Back to Main", null, 16);
+        buttonHome.setOnAction(e -> primaryStage.setScene(splashScene));
+        Button buttonPause = createButton("Pause Simulation", "#3197bc", 16);
+        pauseGame(buttonPause, timeline);
+        Button buttonStop = createButton("Stop Simulation", "#3197bc", 16);
+        stopGame(buttonStop, timeline);
+        Button buttonUpload = createButton("Upload New Simulation", "#3197bc", 16);
+        uploadSim(buttonUpload, timeline);
+        toolBar.getItems().addAll(buttonHome, buttonPause, buttonStop, buttonUpload);
+        return toolBar;
+    }
+
+    private Button createButton(String text, String styleColor, int fontSize) {
+        Button button = new Button(text);
+        button.setTextFill(Color.BLACK);
+        button.setStyle("-fx-background-color:" + styleColor + ";-fx-font-size:" + fontSize + " px;");
+        return button;
+    }
+
+    private void pauseGame(Button buttonPause, Timeline timeline) {
+        buttonPause.setOnAction(e -> timeline.pause());
+    }
+
+    private void stopGame(Button buttonStop, Timeline timeline) {
+        buttonStop.setOnAction(e -> timeline.stop());
+    }
+
+    private void uploadSim(Button buttonUpload, Timeline timeline) {
+        buttonUpload.setOnAction(e -> {
+            timeline.stop();
+        });
+    }
 }
+
