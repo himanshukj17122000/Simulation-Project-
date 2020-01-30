2042fc395bc720e66a0ea350b0e9b38356d6c991
Himanshu Jain <hkj4@dj4.edu>
2020-01-29 22:55:19 -0500

    Configuration added Arrarylist

--- src/cellsociety/XMLReader.java
+++ src/cellsociety/XMLReader.java
@@ -11,6 +11,12 @@ import java.io.File;
 public class XMLReader {
     private static String rows;
     private static String columns;
+    private static String top;
+    private static String bottom;
+    private static String left;
+    private static String right;
+    private static String levels;
+    private static String probCatch;
     public static void main(String argv[]) {
         try {
             File fXmlFile = new File("/Users/himanshu/Desktop/ALL/simulation_team17/resources/file.xml");
@@ -30,19 +36,64 @@ public class XMLReader {
 
                     rows= getTagValue("rows", eElement);
                     columns=getTagValue("columns", eElement);
+                    left= getTagValue("rows", eElement);
+                    right=getTagValue("columns", eElement);
+                    bottom= getTagValue("rows", eElement);
+                    top=getTagValue("columns", eElement);
+                    levels= getTagValue("maxlevels", eElement);
+                    if(getTagValue("probCatch", eElement)!=null){
+                        probCatch=getTagValue("probCatch", eElement);
+                    }
+
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public String getRows(){
-        return rows;
+    public int getRows(){return Integer.parseInt(rows);}
+
+    public int getProbCatch(){
+        if(probCatch!=null) {
+        return Integer.parseInt(probCatch);
+    } return -1;
     }
 
-    public String getColumns(){
-        return columns;
+    public Boolean getLeft(){
+        if(left.equals("No")){
+            return false;
+        }
+        return true;
     }
+
+    public Boolean getRight(){
+        if(right.equals("No")){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean getTop(){
+        if(top.equals("No")){
+            return false;
+        }
+        return true;
+    }
+    public int levels(){
+        return Integer.parseInt(levels);
+    }
+
+    public boolean getBottom(){
+        if(bottom.equals("No")){
+            return false;
+        }
+        return true;
+    }
+
+    public int getColumns(){
+        return Integer.parseInt(columns);
+    }
+
     private static String getTagValue(String sTag, Element eElement) {
         NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
         Node nValue = (Node) nlList.item(0);
