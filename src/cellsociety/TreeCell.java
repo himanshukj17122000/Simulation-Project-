f08ffaf3101730d110ca9c6d7074ba300133616a
Olga Suchankova <os33@duke.edu>
2020-01-30 20:21:59 -0500

    added cell super class

---
+++ src/cellsociety/TreeCell.java
@@ -0,0 +1,13 @@
+package cellsociety;
+
+import javafx.scene.paint.Paint;
+
+public class TreeCell extends Cell{
+    TYPE = Tree
+
+    public TreeCell(double startX, double startY, int width, int height, Paint color){
+        super(startX, startY, width, height, color);
+    }
+
+
+}
