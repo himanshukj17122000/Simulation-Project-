d9db8d7962587817d2ff2efdcade5e17a9c62c2a
Kyra Chan <ktc22@duke.edu>
2020-01-31 21:10:20 -0500

    edit configuration set up

--- src/cellsociety/Configuration/Configuration.java
+++ src/cellsociety/Configuration/Configuration.java
@@ -7,13 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Configuration {
+public abstract class Configuration {
 //    private static final int mySize=400;
 //    private static XMLReader myReader;
-//    private static int myNumRows, myNumColumns, myProbCatch, myNumStates,myIsLeftPresent, myIsRightPresent, myIsTopPresent, myIsBottomPresent;;
-//    private static ArrayList<Integer> myParameters;
-
-
+    private String myTitle,left,right,top,bottom,type1,type2,type3;
+    private int maxStates,rows,columns,neighbours,probCatch;
 
 //    private void checkprob() {
 //        if(myReader.getProbCatch()!=-1){
@@ -34,19 +32,19 @@ public class Configuration {
 ////        parameters.add(myGridArray);
 ////    }
 //
-//    private static int getRandomNumberInRange() {
-//        Random r = new Random();
-//        return r.nextInt(myNumStates + 1);
-//    }
+    private int getRandomNumberInRange() {
+        Random r = new Random();
+        return r.nextInt(this.getMax() + 1);
+    }
 
-    public static List<List<GridEntry>> makeCellGrid() {  // initialization of a grid of empty cells
+    public List<List<GridEntry>> makeCellGrid() {  // initialization of a grid of empty cells
         List<List<GridEntry>> grid = new ArrayList<>();
-        for (int r = 0; r < simulation.myNumRows; r++) {
+        for (int r = 0; r < this.getRows(); r++) {
             List<GridEntry> insertRow = new ArrayList<>();
-            for (int c = 0; c < myNumColumns; c++) {
-                GridEntry insertGridEntry = createBorderGridEntry(r, c, mySimulationType);
+            for (int c = 0; c < this.getColumns(); c++) {
+                GridEntry insertGridEntry = createBorderGridEntry(r, c, this.getTitle());
                 if (insertGridEntry == null) {
-                    insertGridEntry = randomizeGridEntry(r, c, mySimulationType);
+                    insertGridEntry = randomizeGridEntry(r, c, this.getTitle());
                 }
                 insertRow.add(insertGridEntry);
             }
@@ -55,18 +53,18 @@ public class Configuration {
         return grid;
     }
 
-    private static GridEntry createBorderGridEntry(int row, int col, String simulation) {
-        if (myIsBottomPresent != 0 && row == myNumRows + 1) {
-            return new GridEntry(row, col, simulation, simulation.getmyIsBottomPresent);
+    private GridEntry createBorderGridEntry(int row, int col, String simulation) {
+        if (this.getBottom() != 0 && row == this.getRows() + 1) {
+            return new GridEntry(row, col, simulation, this.getBottom());
         }
-        else if (myIsTopPresent != 0 && row == 0) {
-            return new GridEntry(row, col, simulation, myIsTopPresent);
+        else if (this.getTop() != 0 && row == 0) {
+            return new GridEntry(row, col, simulation, this.getTop());
         }
-        else if (myIsLeftPresent != 0 && col == 0) {
-            return new GridEntry(row, col, simulation, myIsLeftPresent);
+        else if (this.getLeft() != 0 && col == 0) {
+            return new GridEntry(row, col, simulation, this.getLeft());
         }
-        else if (myIsRightPresent != 0 && col == myNumColumns + 1) {
-            return new GridEntry(row, col, simulation, myIsRightPresent);
+        else if (this.getRight() != 0 && col == this.getColumns() + 1) {
+            return new GridEntry(row, col, simulation, this.getRight());
         }
         return null;
     }
@@ -75,4 +73,20 @@ public class Configuration {
         int randomType = getRandomNumberInRange();
         return new GridEntry(row, col, simulation, randomType);
     }
+
+    public String getTitle () {
+        return myTitle;
+    }
+    public int getMax(){return maxStates;}
+    public int getRows(){return rows;}
+    public int getColumns(){return columns;}
+    public int getLeft(){return left;}
+    public int getRight(){return right;}
+    public int getTop(){return top;}
+    public int getBottom(){return bottom;}
+    public int getNeighbours(){return neighbours;}
+    public int getProbCatch(){return probCatch;}
+    public String getType1(){return type1;}
+    public String getType2(){return type2;}
+    public String getType3(){return type3;}
 }
