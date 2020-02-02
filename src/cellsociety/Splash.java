b205fa9eed28ba7922472f034c5b01a903092560
Kyra Chan <ktc22@duke.edu>
2020-02-02 13:17:26 -0500

    split visualization class

--- src/cellsociety/Splash.java
+++ src/cellsociety/Splash.java
@@ -1,6 +1,8 @@
 package cellsociety;
 
-import cellsociety.Configuration.*;
+import cellsociety.Configuration.Configuration;
+import cellsociety.Configuration.FileInputException;
+import cellsociety.Configuration.Reader;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.geometry.Orientation;
@@ -20,13 +22,11 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.ObjectInputFilter;
 import java.util.List;
-import java.util.Map;
 
 import static cellsociety.Main.TITLE;
 
-public class Visualization {
+public class Splash {
     public static final int SCREEN_WIDTH = 1200;
     public static final int SCREEN_HEIGHT = 800;
     public static final Paint SCREEN_BACKGROUND = Color.web("1f2e50");
@@ -35,59 +35,28 @@ public class Visualization {
     public static final String BUTTON_STYLE_COLOR = "#3197bc";
     public static final int BUTTON_FONT_SIZE = 16;
 
+    private Scene mySplashScene;
     private Scene myAnimationScene;
-    private Configuration myNewSimulationConfig;
+    private Configuration simulationConfig;
 
-    public Visualization(Stage primaryStage, Timeline timeline, Configuration simulationConfig) {
-        myAnimationScene = buildAnimationScene(primaryStage, timeline, simulationConfig);
+    public Splash(Stage primaryStage, Timeline timeline) {
+        mySplashScene = buildSplashScene(primaryStage, timeline);
     }
 
-    public Scene getMyAnimationScene() {
-        return myAnimationScene;
+    public Scene getMySplashScene() {
+        return mySplashScene;
     }
+    public Configuration getSimulationConfig() { return simulationConfig; }
 
-    public GridPane drawGrid(List<List<GridEntry>> cellStates) {
-        GridPane grid = new GridPane();
-        grid.setPrefSize(GRID_WIDTH,GRID_HEIGHT);
-        grid.setStyle("-fx-padding: 10; -fx-border-style: solid inside; -fx-border-width: 2; -fx-border-insets: 5; " +
-                "-fx-border-radius: 5; -fx-border-color: black;");
-        for (int row = 0; row < cellStates.size(); row += 1) {
-            for (int col = 0; col < cellStates.get(row).size(); col += 1) {
-                Cell cell = cellStates.get(row).get(col).getCell();
-                grid.add(cell.getRectangle(), row, col);
-            }
-        }
-        return grid;
-    }
-
-    private Scene buildAnimationScene(Stage primaryStage, Timeline timeline, Configuration simulationConfig) {
-        ToolBar toolBar = buildToolBar(primaryStage, timeline, simulationConfig);
-        HBox root = new HBox();
-        GridPane grid = initializeGrid(simulationConfig);
-        root.getChildren().addAll(toolBar, grid);
-        return new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
-    }
-
-    private GridPane initializeGrid(Configuration simulationConfig) {
-        List<List<GridEntry>> cellStates = simulationConfig.makeCellGrid();
-        return drawGrid(cellStates);
-    }
-
-    private ToolBar buildToolBar(Stage primaryStage, Timeline timeline, Configuration simulationConfig) {
-        ToolBar toolBar = new ToolBar();
-        toolBar.setOrientation(Orientation.VERTICAL);
-        Slider probabilitySlider = createSlider(simulationConfig.getProbCatch());
-        Button buttonHome = createButton("Back to Main", null, BUTTON_FONT_SIZE);
-        Splash home = new Splash(primaryStage, timeline);
-        buttonHome.setOnAction(e -> primaryStage.setScene(home.getMySplashScene()));
-        Button buttonPause = createButton("Pause Simulation", BUTTON_STYLE_COLOR, BUTTON_FONT_SIZE);
-        pauseGame(buttonPause, timeline);
-        Button buttonStop = createButton("Stop Simulation", BUTTON_STYLE_COLOR, BUTTON_FONT_SIZE);
-        stopGame(buttonStop, timeline);
+    private Scene buildSplashScene(Stage primaryStage, Timeline timeline) {
+        Text simTitle = new Text(TITLE);
+        simTitle.setTextAlignment(TextAlignment.CENTER);
         Button buttonUpload = createButton("Upload New Simulation", BUTTON_STYLE_COLOR, BUTTON_FONT_SIZE);
+        buttonUpload.setLayoutX(550);
         uploadSim(buttonUpload, primaryStage, timeline);
-        toolBar.getItems().addAll(probabilitySlider, buttonHome, buttonPause, buttonStop, buttonUpload);
-        return toolBar;
+        VBox root = new VBox(20);
+        root.getChildren().addAll(simTitle, buttonUpload);
+        return new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BACKGROUND);
     }
 
     private Button createButton(String text, String styleColor, int fontSize) {
@@ -95,27 +64,6 @@ public class Visualization {
         button.setTextFill(Color.BLACK);
         button.setStyle("-fx-background-color:" + styleColor + ";-fx-font-size:" + fontSize + " px;");
         return button;
-    }
-
-    private Slider createSlider(double defaultValue) {
-        Slider slider = new Slider();
-        slider.setMin(0);
-        slider.setMax(1);
-        slider.setValue(defaultValue);
-        slider.setShowTickLabels(true);
-        slider.setShowTickMarks(true);
-        slider.setMajorTickUnit(0.5);
-        slider.setMinorTickCount(5);
-        slider.setBlockIncrement(0.05);
-        return slider;
-    }
-
-    private void pauseGame(Button buttonPause, Timeline timeline) {
-        buttonPause.setOnAction(e -> timeline.pause());
-    }
-
-    private void stopGame(Button buttonStop, Timeline timeline) {
-        buttonStop.setOnAction(e -> timeline.stop());
     }
 
     private void uploadSim(Button buttonUpload, Stage primaryStage, Timeline timeline) {
@@ -126,7 +74,7 @@ public class Visualization {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-            Visualization animation = new Visualization(primaryStage, timeline, myNewSimulationConfig);
+            Visualization animation = new Visualization(primaryStage, timeline, simulationConfig);
             primaryStage.setScene(animation.getMyAnimationScene());
         });
     }
@@ -143,27 +91,27 @@ public class Visualization {
 //                        Pair<String, Game> p = new Pair<>(dataFile.getName(), new XMLParser("media").getGame(dataFile));
 //                        showMessage(AlertType.INFORMATION, p.getSecond().toString());
                     new Reader("type").getFire(dataFile);
-                    myNewSimulationConfig = Configuration.getFireClass();
+                    simulationConfig = Configuration.getFireClass();
                 }
                 else if(dataFile.getName().equals("gameOfLife.xml")){
                     //simulationConfig = new Game();
                     new Reader("type").getGame(dataFile);
-                    myNewSimulationConfig = Configuration.getGameClass();
+                    simulationConfig = Configuration.getGameClass();
                 }
                 else if(dataFile.getName().equals("percolation.xml")){
                     //simulationConfig = new Percolation();
                     new Reader("type").getPercolation(dataFile);
-                    myNewSimulationConfig = Configuration.getPerClass();
+                    simulationConfig = Configuration.getPerClass();
                 }
                 else if(dataFile.getName().equals("prey.xml")){
                     //simulationConfig = new Prey();
                     new Reader("type").getPrey(dataFile);
-                    myNewSimulationConfig = Configuration.getPreyClass();
+                    simulationConfig = Configuration.getPreyClass();
                 }
                 else if(dataFile.getName().equals("segregation.xml")){
                     //simulationConfig = new Segregation();
                     new Reader("type").getSegregation(dataFile);
-                    myNewSimulationConfig = Configuration.getSegClass();
+                    simulationConfig = Configuration.getSegClass();
                 }
 
             }
