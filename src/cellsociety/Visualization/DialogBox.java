f08e5f8ad46de21664e6bf3635b88a79f9813c77
Himanshu Jain <himanshu.jain@duke.edu>
2020-02-06 19:13:43 -0500

    Revert "Changed the Configuration folder"
    This reverts commit 2106120cc5a1b00d3e4ac2e6027e65bca5d79418

--- src/cellsociety/Visualization/DialogBox.java
+++ src/cellsociety/Visualization/DialogBox.java
@@ -1,76 +1,52 @@
 package cellsociety.Visualization;
 
-import cellsociety.Configuration.*;
+import cellsociety.Configuration.Configuration;
+import cellsociety.Configuration.FileInputException;
+import cellsociety.Configuration.Reader;
 import cellsociety.Visualization.Visualization;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 
 
 public class DialogBox {
     public static final String DATA_FILE_EXTENSION = "*.xml";
-    private static final String FIRE_FILE = "Fire";
-    private static final String GAME_FILE = "Game of Life";
-    private static final String PERC_FILE = "Percolation";
-    private static final String PREY_FILE = "Prey";
-    private static final String SEG_FILE = "Segregation";
+    private static final String FIRE_FILE = "fire.xml";
+    private static final String GAME_FILE = "gameOfLife.xml";
+    private static final String PERC_FILE = "percolation.xml";
+    private static final String PREY_FILE = "prey.xml";
+    private static final String SEG_FILE = "segregation.xml";
     private static final String TYPE = "type";
-    private static String title;
-    private static Map<String,String> result= new HashMap<>();
     // NOTE: generally accepted behavior that the chooser remembers where user left it last
     public final static FileChooser FILE_CHOOSER = makeChooser(DATA_FILE_EXTENSION);
     private Configuration mySimulationConfig;
 
     public void start(Stage primaryStage, Configuration simConfig) throws NullPointerException {
         File dataFile = FILE_CHOOSER.showOpenDialog(primaryStage);
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(dataFile);
-        doc.getDocumentElement().normalize();
-        NodeList nList = doc.getElementsByTagName("gridlayout");
-            Node nNode = nList.item(0);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                title= getTagValue("title", eElement);
-            }
         try {
-            switch (title) {
+            switch (dataFile.getName()) {
                 case FIRE_FILE:
-                    result.clear();
-                    result= new Reader(TYPE).getSimulation(dataFile);
-                    simConfig = new Fire(result);
+                    new Reader(TYPE).getFire(dataFile);
+                    simConfig = Configuration.getFireClass();
                     break;
                 case GAME_FILE:
-                    result.clear();
-                    result= new Reader(TYPE).getSimulation(dataFile);
-                    simConfig = new Game(result);
+                    new Reader(TYPE).getGame(dataFile);
+                    simConfig = Configuration.getGameClass();
                     break;
                 case PERC_FILE:
-                    result.clear();
-                    result= new Reader(TYPE).getSimulation(dataFile);
-                    simConfig = new Percolation(result);
+                    new Reader(TYPE).getPercolation(dataFile);
+                    simConfig = Configuration.getPerClass();
                     break;
                 case PREY_FILE:
-                    result.clear();
-                    result= new Reader(TYPE).getPrey(dataFile);
-                    simConfig = new Prey(result);
+                    new Reader(TYPE).getPrey(dataFile);
+                    simConfig = Configuration.getPreyClass();
                     break;
                 case SEG_FILE:
-                    result.clear();
-                    result= new Reader(TYPE).getSimulation(dataFile);
-                    simConfig = new Segregation(result);
+                    new Reader(TYPE).getSegregation(dataFile);
+                    simConfig = Configuration.getSegClass();
                     break;
             }
             Visualization animation = new Visualization(primaryStage, simConfig);
@@ -100,11 +76,4 @@ public class DialogBox {
 
     // Getter method for configuration to be passed on in Visualization and Simulation
     public Configuration getSimulationConfig() { return mySimulationConfig; }
-
-
-    private static String getTagValue(String sTag, Element eElement) {
-        NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
-        Node nValue = (Node) nlList.item(0);
-        return nValue.getNodeValue();
-    }
 }
