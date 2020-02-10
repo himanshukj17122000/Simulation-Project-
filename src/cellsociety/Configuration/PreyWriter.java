dad029f662b86f2d371364834f70baa18e3cac76
Himanshu Jain <hkj4@dj4.edu>
2020-02-10 01:19:42 -0500

    Done with PreyWriter

--- src/cellsociety/Configuration/PreyWriter.java
+++ src/cellsociety/Configuration/PreyWriter.java
@@ -26,16 +26,24 @@ public class PreyWriter {
             icBuilder = icFactory.newDocumentBuilder();
             Document doc = icBuilder.newDocument();
             Element mainRootElement;
-            double probcatch=0;
+            double prob=0;
+            double sharkTime=0;
+            double dayswithoutFood=0;
+            ArrayList<Double> constants= new ArrayList<>();
             for(ProbConstant c:myNewProbCatch.values()){
-                probcatch=c.getMyProbCatch();
+                constants.add(c.getMyProbCatch());
             }
-            System.out.println(savedFile);
+            prob=constants.get(0);
+            System.out.println(prob);
+            sharkTime=constants.get(1);
+            System.out.println(sharkTime);
+            dayswithoutFood=constants.get(2);
+            System.out.println(dayswithoutFood);
+
             mainRootElement = doc.createElement("simulation");
             Map<String, Integer> concentrationMap= mySim.getTypesOfCells();
             ArrayList<Integer> numberofEach= new ArrayList<>();
             for(String typeOfCell:concentrationMap.keySet()){
-
                 numberofEach.add(concentrationMap.get(typeOfCell));
             }
             Integer type1;
@@ -49,7 +57,7 @@ public class PreyWriter {
             conc=per1+","+per2;
             mainRootElement.setAttribute("type", simulationConfig.getTitle());
             doc.appendChild(mainRootElement);
-            mainRootElement.appendChild(getCompany(doc,simulationConfig.getTitle(), simulationConfig, probcatch));
+            mainRootElement.appendChild(getCompany(doc,simulationConfig.getTitle(), simulationConfig, prob,sharkTime,dayswithoutFood));
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             DOMSource source = new DOMSource(doc);
@@ -60,29 +68,35 @@ public class PreyWriter {
         }
     }
 
-    private static Node getCompany(Document doc, String title, Configuration simulation, double prob) {
-//        "title", "maxStates", "rows","columns","left","right",
-//                "top","bottom","neighbours","type1","type2","type3","fishBreed","sharkBreed","fishLabel","sharkLabel","numLabel","maxFishBreed"
-//                ,"maxSharkBreed","numWithoutFood","maxDays","neighPattern","shape","concentration","initial","colors","boundary"
+    private static Node getCompany(Document doc, String title, Configuration simulation, double prob,double shark, double days) {
         Element company = doc.createElement("gridlayout");
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(0), title));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(1), String.valueOf(simulation.getMaxStates())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(2), String.valueOf(simulation.getRows())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(3), String.valueOf(simulation.getColumns())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(4), String.valueOf(simulation.getLeft())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(5), String.valueOf(simulation.getRight())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(6), String.valueOf(simulation.getTop())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(7), String.valueOf(simulation.getBottom())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(8), String.valueOf(simulation.getNeighbours())));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(9), simulation.getType1()));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(10),simulation.getType2()));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(11), simulation.getType3()));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(12), simulation.getNeighPatterninString()));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(13), simulation.getShape()));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(14), conc));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(15), "Given"));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(16), simulation.getColors().get(0)));
-        company.appendChild(getCompanyElements(doc, company, Game.DATA_FIELDS.get(17), simulation.getMyboundary()));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(0), title));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(1), String.valueOf(simulation.getMaxStates())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(2), String.valueOf(simulation.getRows())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(3), String.valueOf(simulation.getColumns())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(4), String.valueOf(simulation.getLeft())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(5), String.valueOf(simulation.getRight())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(6), String.valueOf(simulation.getTop())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(7), String.valueOf(simulation.getBottom())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(8), String.valueOf(simulation.getNeighbours())));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(9), simulation.getType1()));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(10),simulation.getType2()));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(11), simulation.getType3()));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(12), String.valueOf(prob)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(13), String.valueOf(shark)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(14), simulation.getProbCatchLabel().get(0)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(15), simulation.getProbCatchLabel().get(1)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(16), simulation.getProbCatchLabel().get(2)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(17), String.valueOf(simulation.getMaxProb().get(0))));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(18), String.valueOf(simulation.getMaxProb().get(1))));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(19), String.valueOf(days)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(20), String.valueOf(simulation.getMaxProb().get(2))));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(21), simulation.getNeighPatterninString()));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(22), simulation.getShape()));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(23), conc));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(24), "Given"));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(25), simulation.getColors().get(0)));
+        company.appendChild(getCompanyElements(doc, company, Prey.DATA_FIELDS.get(26), simulation.getMyboundary()));
         return company;
     }
 
